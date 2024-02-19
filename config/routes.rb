1889c3bc167fc0fec90edab371a56692b544c061
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-02-19 20:08:01 +0000

    Rails generate devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "boards", :action => "index" })
+
+  root to: "boards#index"
+  # this replaces line 6 (same thing) but for devise
+
+  # get("/", { :controller => "boards", :action => "index" })
 
   # Routes for the Post resource:
 
